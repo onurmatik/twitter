66a12136a327d2fb54acf49c584965b7da9a75b1
onurmatik <omat@gezgin.com>
2016-09-02 12:21:58 +0300

    added twitter user admin

--- twitter/users/admin.py
+++ twitter/users/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from twitter.users.models import TwitterUser
 
-# Register your models here.
+
+@admin.register(TwitterUser)
+class TwitterUserAdmin(admin.ModelAdmin):
+    search_fields = ('data', 'id')
+    list_display = ('name', 'time', 'protected', 'deactivated',)
+    list_filter = ('protected', 'deactivated',)
