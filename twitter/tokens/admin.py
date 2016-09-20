813081817837a1a1fb695ab086e2c583d5be1c53
onurmatik <omat@gezgin.com>
2016-09-20 16:26:04 +0300

    admin improvements

--- twitter/tokens/admin.py
+++ twitter/tokens/admin.py
@@ -11,3 +11,5 @@ class ApplicationAdmin(admin.ModelAdmin):
 class TokenAdmin(admin.ModelAdmin):
     list_display = ('__unicode__', 'app', 'valid')
     list_editable = ('app',)
+    list_filter = ('valid',)
+    search_fields = ('data',)
