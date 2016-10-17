98329bfb0a2466d363953b7fe6e739de822fcc20
onurmatik <omat@gezgin.com>
2016-10-17 17:00:08 +0300

    create lists via admin

--- twitter/lists/admin.py
+++ twitter/lists/admin.py
@@ -1,5 +1,52 @@
+from django import forms
 from django.contrib import admin
+from django.conf import settings
+from twitter.api import AppClient
 from twitter.lists.models import List
+from twitter.tokens.models import Token
+
+
+class ListForm(forms.ModelForm):
+    name = forms.CharField(required=False)
+    members = forms.CharField(required=False, widget=forms.Textarea)
+    mode = forms.ChoiceField(choices=(('public', 'public'), ('private', 'private')))
+
+    class Meta:
+        model = List
+
+    def save(self, **kwargs):
+        if self.members:
+            # create a new Twitter list
+            client = AppClient(
+                consumer_key=settings.CONSUMER_KEY,
+                consumer_secret=settings.CONSUMER_SECRET,
+            )
+            response = client.api.lists.create.post(
+                name=self.name,
+                mode=self.mode,
+            )
+            list_id = response.data['id']
+            while len(self.members) > 0:
+                members = self.members[:100]
+                del(self.members[:100])
+                response = client.api.lists.members.create_all.post(
+                    list_id=list_id,
+                    screen_name=members,
+                )
+        else:
+            # URL; fetch an existing Twitter list
+            owner_screen_name, slug = self.name.split('/')[-2:]
+            token = Token.objects.get_for_resource('/lists/show')
+            if token:
+                client = token.get_client()
+                response = client.api.lists.show(
+                    owner_screen_name=owner_screen_name,
+                    slug=slug,
+                )
+                List.objects.create(
+                    id=response.data['id'],
+                    data=response.data,
+                )
 
 
 @admin.register(List)
